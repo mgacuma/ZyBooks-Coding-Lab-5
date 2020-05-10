2b36f2225b06d177f071d3ccdc328500e718ecd6
mgacuma <60759812+mgacuma@users.noreply.github.com>
2020-05-10 03:23:16 -0700

    Update ListADT.h

--- ListADT.h
+++ ListADT.h
@@ -1,32 +1,53 @@
 #ifndef LISTADT_H
 #define LISTADT_H
 
-#include<string>
+#include <string>
+#include<iostream>
+
 using namespace std;
 
 class Node {
-   public: 
-     Node(){   };  
-    //implement this default constructor as an inline function using an initialization section. 0->value, nullptr->next 
-     Node(int val) {  } ; 
-     //implement this constructor as an inline function using an initialization section. val->value, nullptr->next   
+   public:
+     Node(){
+    	 value = 0;
+    	 next = nullptr;
+     }
+    //implement this default constructor as an inline function using an initialization section. 0->value, nullptr->next
+     Node(int val){
+		value = val;
+		next = nullptr;
+     }
+     //implement this constructor as an inline function using an initialization section. val->value, nullptr->next
      int value;
-     Node *next
+     Node* next;
 };
-
 
 class ListADT {
+	friend ostream& operator<<(ostream& os, const ListADT& l);
+
   public:
-      void push_back(int val);
-      void push_frant(int val);
-      void pop_back();
-      void pop_front();
-      operator[]();
-      operator<<();
-      int length() const;
-      
+    ListADT();
+    ListADT(const ListADT& origClass);
+    ~ListADT();
+    void push_back(int val);
+    void push_front(int val);
+    void pop_back();
+    void pop_front();
+
+
+    ListADT& operator=(const ListADT& origClass);
+    int& operator[](int i);
+
+
+    int length() const;
+
   private:
-      Node *head; //point to the first node on the linked list
-      int size;  //number of nodes on the linked list
+    Node* head; //point to the first node on the linked list
+    int size;  //number of nodes on the linked list
+
 };
+
+
+
+
 #endif

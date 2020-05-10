44232203ebbd5c07ef60852f889312ea43cdc2b9
mgacuma <60759812+mgacuma@users.noreply.github.com>
2020-05-10 03:23:51 -0700

    Update VectorADT.h

--- VectorADT.h
+++ VectorADT.h
@@ -2,24 +2,32 @@
 #define VECTORADT_H
 
 #include <string>
-#include <vector>
+#include<iostream>
+
 using namespace std;
 
 class VectorADT {
-  public:
-    VectorADT();
-    ~VectorADT();
-    void push_back(double val);
-    void resize(int newSize);
-    void pop_back();
-    operator[]();
-    operator+();
-    operator<<();
-    int length() const;
-    int curr_capacity() const;
-  private:
-    double * array;
-    int size;    //the number of doubles stored in array
-    int capacity;  //the maximum number of doubles that can be stored in array
+
+	friend ostream& operator<<(ostream& os, const VectorADT& v);
+
+	public:
+
+		VectorADT();
+		VectorADT(const VectorADT& origClass);
+		~VectorADT();
+		void push_back(double val);
+		void resize(int newSize);
+		void pop_back();
+		VectorADT& operator=(const VectorADT& origClass);
+		double& operator[](int i);
+		VectorADT operator+(const VectorADT& rhs);
+		int length() const;
+		int curr_capacity() const;
+
+	private:
+
+		double * array;
+		int size;    //the number of doubles stored in array
+		int capacity;  //the maximum number of doubles that can be stored in array
 };
 #endif
